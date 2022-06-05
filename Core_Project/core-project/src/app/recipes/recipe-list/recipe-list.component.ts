61d3b47a1a585af6b5c78b725ac0d5a59eb6c128
Daniel Murphy <daniel.murphy@capco.com>
2022-06-05 15:39:01 -0400

    built out recipe modal and have a dummy recipe displaying

--- Core_Project/core-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Core_Project/core-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.modal';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is a Test recipe',
+      'https://cdn.pixabay.com/photo/2020/02/02/15/07/meat-4813261_1280.jpg'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'This is a Test recipe',
+      'https://cdn.pixabay.com/photo/2020/02/02/15/07/meat-4813261_1280.jpg'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
